3f5a603a696d6579680a50d9d10b3ea66c81bfba
Petr Kočí <petr.koci@samizdat.cz>
2024-06-28 16:19:38 +0200

    fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import { tsvParseRows } from "d3-dsv";
+import { Switch } from "@/components/ui/switch"
+import { Label } from "@/components/ui/label"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [simpleData, setSimpleData] = useState<[string, number][]>([]);
+  const [simpleLoaded, setSimpleLoaded] = useState(false);
+  const [showSimple, setShowSimple] = useState(true);
+
+  const [complexData, setComplexData] = useState<[string, number][]>([]);
+  const [complexLoaded, setComplexLoaded] = useState(false);
+  const [showComplex, setShowComplex] = useState(false);
+
+  const fetchData = async (url: string) => {
+    const response = await fetch(url);
+    const text = await response.text();
+    const parsed = await tsvParseRows(text);
+    const data = await parsed.map((d) => [d[0], +d[1]]);
+    return data;
+  };
+
+  const toggleChecked = (checked: boolean, which: string) => () => {
+    if (which === "simple") { setShowSimple(checked); }
+    if (which === "complex") {
+      if (!complexLoaded) {
+        const data = fetchData("data/namesComplex.tsv");
+        data.then((d) => {
+          setComplexData(d as [string, number][]);
+          setComplexLoaded(true);
+        });
+      }
+      setShowComplex(checked);
+    }
+  }
+
+  useEffect(() => {
+    if (!simpleLoaded) {
+      const data = fetchData("data/namesSimple.tsv");
+      data.then((d) => {
+        setSimpleData(d as [string, number][]);
+        setSimpleLoaded(true);
+      });
+    }
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="space-y-2">
+      <div className="flex items-center space-x-2">
+        <Switch id="simple-names" checked={showSimple} onCheckedChange={toggleChecked(!showSimple, "simple")} />
+        <Label htmlFor="simple-names">Jednoslovná jména, např. Marie</Label>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="flex items-center space-x-2">
+        <Switch id="complex-names" checked={showComplex} onCheckedChange={toggleChecked(!showComplex, "complex")} />
+        <Label htmlFor="complex-names">Složená jména, např. Anna Marie</Label>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>)
 }
 
 export default App
